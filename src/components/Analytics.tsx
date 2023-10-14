7c9a59d250dc2bd0cba0e646721ddd1db7e48941
Vincent Dusautoir <vincent.dusautoir.pro@gmail.com>
2023-10-14 18:03:07 +0200

    feat: analytic script update

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -7,7 +7,7 @@ export const Analytics = () => {
         async
         defer
         data-website-id="277d9524-3bf1-4ae1-958a-85690c325cfd"
-        src="https://analytics.vincentdusautoir.com/umami.js"
+        src="https://analytics.vincentdusautoir.com/script.js"
         data-do-not-track="true"
         data-domains="vincentdusautoir.com"
       />
